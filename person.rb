b9348db77433529ed4c52a961ac73accef23e8bf
SOM ON Soum <somonmel07@gmail.com>
2017-12-29 03:56:16 +0700

    Object Oriented Programming in Ruby

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
 # I have a Person class
+
 class Person
 
-  attr_accessor :name, :email #You don't need to use method get and set
+  attr_accessor :name, :email #You don't need to use get and set method
 
   def initialize(name, email)
     @name = name
@@ -23,20 +24,39 @@ class Person
   end
 end
 
-seller = Person.new("Meas Dara", "darameas@gmail.com")
-buyer = Person.new("Sok Dara", "darasok@gmail.com")
+
+class Teacher < Person
+
+end
+
+class Student < Person
+
+end
+
+class Supervisor < Person
+
+end
+
+person = Person.new("Meas Dara", "darameas@gmail.com")
+person1 = Person.new("Sok Dara", "darasok@gmail.com")
 
 =begin
-puts seller
-puts buyer
+puts person
+puts person
 =end
 
-puts"My name is #{seller.name} and my email is #{seller.email}"
+puts "My name is #{person.name} and my email is #{person.email}"
 # Syntax to use attr_accessor instead of get and set method
-seller.name = "Pros Sok"
-seller.email = "sokpross@gmail.com"
+person.name = "Pros Sok"
+person.email = "sokpross@gmail.com"
 puts "\n"
-puts"My new name is #{seller.name} and my new email is #{seller.email} "
+puts "My new name is #{person.name} and my new email is #{person.email} "
 
-# puts seller.get_name + " can run #{seller.run(1, 3)} m/s"
-# puts buyer.set_name("John") + " can run #{buyer.run(3, 5)} m/s"
+# puts person.get_name + " can run #{person.run(1, 3)} m/s"
+# puts person.set_name("John") + " can run #{person.run(3, 5)} m/s"
+
+teacher = Teacher.new("TAL Tongsreng", "tongsrengtal@gmail.com")
+
+puts "My new name is #{teacher.name} and my new email is #{teacher.email}"
+
+puts "I can run #{teacher.run(2, 4)}"
